d28a7ca3f7a6b809799aebdc4abf848ab9f58306
Hadwin <linnaiwei@gmail.com>
2020-03-27 15:20:01 -0700

    Create a web server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
-func main(){
-	fmt.Println("Hello world!!!")
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello world</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
