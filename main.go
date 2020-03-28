c7a1f021331fc0467441f867fc587a9aca2fd005
Hadwin <linnaiwei@gmail.com>
2020-03-29 04:24:58 +0800

    add faq page

--- main.go
+++ main.go
@@ -14,13 +14,18 @@ func home(w http.ResponseWriter, r *http.Request) {
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<address>To get support, please contact to my <a href=\"mailto:linnaiwei@gmail.com\">email</a></address>")
+}
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>This is my FAQ page</h1>")
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.Handle("/", r)
 	http.ListenAndServe(":3000", nil)
 }
