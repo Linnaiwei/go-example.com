8342f73d0956d2f9c1fe88c7d2f192bce4cce874
Hadwin <linnaiwei@gmail.com>
2020-03-28 21:56:12 +0800

    Add a router to contact page

--- main.go
+++ main.go
@@ -7,7 +7,12 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Hello world</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello world</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "<address>To get support, please contact to my <a href=\"mailto:linnaiwei@gmail.com\">email</a></address>")
+	}
+	
 }
 
 func main() {
