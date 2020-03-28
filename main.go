5cb78b4b59fbe0dfe5e5d2a53c7d5e50e3ae5242
Hadwin <linnaiwei@gmail.com>
2020-03-29 04:11:34 +0800

    implement mux router

--- main.go
+++ main.go
@@ -3,22 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"github.com/julienschmidt/httprouter"
+	"github.com/gorilla/mux"
 )
 
-//Index  This function will return a welcome page
-func Index(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "<h1>Welcome! My friend ^o^</h1>")
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Hello world</h1>")
 }
 
-//Hello  This function will say hello to the name you write in the named parameters
-func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprint(w, "<h1>Hello! ", ps.ByName("name") ," :) </h1>")
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<address>To get support, please contact to my <a href=\"mailto:linnaiwei@gmail.com\">email</a></address>")
+
 }
 
 func main() {
-	router := httprouter.New()
-    router.GET("/", Index)
-    router.GET("/hello/:name", Hello)
-	http.ListenAndServe(":3000", router)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.Handle("/", r)
+	http.ListenAndServe(":3000", nil)
 }
