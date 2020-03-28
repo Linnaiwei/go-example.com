8e9e74c9b4669f4e3c6957f72c7650250e21b838
Hadwin <linnaiwei@gmail.com>
2020-03-29 06:21:30 +0800

    add a 404 page

--- main.go
+++ main.go
@@ -21,11 +21,17 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>This is my FAQ page</h1>")
 }
 
+func notFoundPage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>Wo could not find the page you were going >_< </h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
+	r.NotFoundHandler = http.HandlerFunc(notFoundPage)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
-	http.Handle("/", r)
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
